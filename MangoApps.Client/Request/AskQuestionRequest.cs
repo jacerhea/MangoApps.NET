6012aff2263b7c6cdd2fe158ff3c0467a3f48603
Jace <jace.rhea@gmail.com>
2015-10-12 23:39:00 -0500

    documenation

--- MangoApps.Client/Request/AskQuestionRequest.cs
+++ MangoApps.Client/Request/AskQuestionRequest.cs
@@ -3,6 +3,9 @@ using System.Runtime.Serialization;
 
 namespace MangoApps.Client.Request
 {
+    /// <summary>
+    /// A request to ask a question amongst your followers or within project or group.
+    /// </summary>
     [DataContract]
     public class AskQuestionRequest
     {
